347e38227c60cb4c821ba8c0519d6e390c35d704
Salahov-Ilia <salahov.ilia@mail.ru>
2016-02-28 13:28:51 +0300

    new version

---
+++ GameOver.pas
@@ -0,0 +1,27 @@
+unit GameOver;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    Label2: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
